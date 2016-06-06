61e768084a1806904f088d06d8147eb2a604bbe6
xiaoerge <xiaoerge616@aol.com>
2016-06-06 02:12:45 -0400

    information source unit test

--- src/main/java/com/xiaoerge/healthcare/x12/level/InformationSource.java
+++ src/main/java/com/xiaoerge/healthcare/x12/level/InformationSource.java
@@ -30,6 +30,32 @@ public class InformationSource implements IMessage {
     }
 
     public String toX12String() {
-        return hierarchicalLevel.toX12String()+"~"+individualOrOrganizationalName.toX12String()+"~";
+        return hierarchicalLevel.toX12String()+individualOrOrganizationalName.toX12String();
     }
+
+    public String getHierarchicalIDNumber() { return hierarchicalLevel.getHierarchicalIDNumber(); }
+    public String getHierarchicalLevelCode() { return hierarchicalLevel.getHierarchicalLevelCode(); }
+    public String getHierarchicalChildCode() { return hierarchicalLevel.getHierarchicalChildCode(); }
+
+    public String getEntityIdentifierCode() { return individualOrOrganizationalName.getEntityIdentifierCode(); }
+    public String getEntityTypeQualifier() { return individualOrOrganizationalName.getEntityTypeQualifier(); }
+    public String getNameLastOrOrganizationName() { return individualOrOrganizationalName.getNameLastOrOrganizationName(); }
+    public String getNameFirst() { return individualOrOrganizationalName.getNameFirst(); }
+    public String getNameMiddle() { return individualOrOrganizationalName.getNameMiddle(); }
+    public String getNameSuffix() { return individualOrOrganizationalName.getNameSuffix(); }
+    public String getIdentificationCodeQualifier() { return individualOrOrganizationalName.getIdentificationCodeQualifier(); }
+    public String getIdentificationCode() { return individualOrOrganizationalName.getIdentificationCode(); }
+
+    public void setHierarchicalIDNumber(String s) { hierarchicalLevel.setHierarchicalIDNumber(s); }
+    public void setHierarchicalLevelCode(String s) { hierarchicalLevel.setHierarchicalLevelCode(s); }
+    public void setHierarchicalChildCode(String s) { hierarchicalLevel.setHierarchicalChildCode(s); }
+
+    public void setEntityIdentifierCode(String s) { individualOrOrganizationalName.setEntityIdentifierCode(s); }
+    public void setEntityTypeQualifier(String s) { individualOrOrganizationalName.setEntityTypeQualifier(s); }
+    public void setNameLastOrOrganizationName(String s) { individualOrOrganizationalName.setNameLastOrOrganizationName(s); }
+    public void setNameFirst(String s) { individualOrOrganizationalName.setNameFirst(s); }
+    public void setNameMiddle(String s) { individualOrOrganizationalName.setNameMiddle(s); }
+    public void setNameSuffix(String s) { individualOrOrganizationalName.setNameSuffix(s); }
+    public void setIdentificationCodeQualifier(String s) { individualOrOrganizationalName.setIdentificationCodeQualifier(s); }
+    public void setIdentificationCode(String s) { individualOrOrganizationalName.setIdentificationCode(s); }
 }
--- src/test/java/com/xiaoerge/healthcare/x12/InformationSourceTest.java
+++ src/test/java/com/xiaoerge/healthcare/x12/InformationSourceTest.java
@@ -12,17 +12,34 @@ public class InformationSourceTest {
 
     @Test
     public void testParseInformationSource() {
-        String x12 = "HL*1**20*1~NM1*1P*1*JONES*MARCUS*A*MR*MD*34*111223333*AA*BB*C~";
+        String x12 = "HL*1**20*1~NM1*PR*2*ACE INSURANCE COMPANY*****PI*87728~";
         InformationSource informationSource = new InformationSource(x12);
 
         Assert.assertTrue(informationSource.validate());
         Assert.assertEquals(x12, informationSource.toX12String());
+        Assert.assertEquals("1", informationSource.getHierarchicalIDNumber());
+        Assert.assertEquals("20", informationSource.getHierarchicalLevelCode());
+        Assert.assertEquals("1", informationSource.getHierarchicalChildCode());
+        Assert.assertEquals("PR", informationSource.getEntityIdentifierCode());
+        Assert.assertEquals("2", informationSource.getEntityTypeQualifier());
+        Assert.assertEquals("ACE INSURANCE COMPANY", informationSource.getNameLastOrOrganizationName());
+        Assert.assertEquals("PI", informationSource.getIdentificationCodeQualifier());
+        Assert.assertEquals("87728", informationSource.getIdentificationCode());
     }
 
     @Test
     public void testCreateInformationSource() {
-        String x12 = "HL*1**20*1~NM1*1P*1*JONES*MARCUS*A*MR*MD*34*111223333*AA*BB*C~";
+        String x12 = "HL*1**20*1~NM1*PR*2*ACE INSURANCE COMPANY*****PI*87728~";
         InformationSource informationSource = new InformationSource();
+
+        informationSource.setHierarchicalIDNumber("1");
+        informationSource.setHierarchicalLevelCode("20");
+        informationSource.setHierarchicalChildCode("1");
+        informationSource.setEntityIdentifierCode("PR");
+        informationSource.setEntityTypeQualifier("2");
+        informationSource.setNameLastOrOrganizationName("ACE INSURANCE COMPANY");
+        informationSource.setIdentificationCodeQualifier("PI");
+        informationSource.setIdentificationCode("87728");
 
         Assert.assertTrue(informationSource.validate());
         Assert.assertEquals(x12, informationSource.toX12String());

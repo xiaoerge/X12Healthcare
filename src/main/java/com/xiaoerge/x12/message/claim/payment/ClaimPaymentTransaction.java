9d936dfb40510058cf394aa7685589c4360f3fbd
xiaoerge <xiaoerge616@aol.com>
2016-11-16 21:57:22 -0500

    merge remote

--- src/main/java/com/xiaoerge/x12/message/claim/payment/ClaimPaymentTransaction.java
+++ src/main/java/com/xiaoerge/x12/message/claim/payment/ClaimPaymentTransaction.java
@@ -1,14 +1,29 @@
 package com.xiaoerge.x12.message.claim.payment;
 
 import com.xiaoerge.x12.message.control.Transaction;
+import com.xiaoerge.x12.message.segment.*;
+import com.xiaoerge.x12.util.StringQueue;
 
 /**
  * Created by xiaoerge on 9/3/16.
  */
 public class ClaimPaymentTransaction extends Transaction {
 
+    private BPR financialInformation;
+    private TRN reassociationTraceNumber;
+    private CUR foreignCurrencyInformation;
+    private REF receiverIdentification;
+    private REF versionIdentification;
+    private DTM productionDate;
+
     public ClaimPaymentTransaction() {
         super();
+        financialInformation = new BPR();
+        reassociationTraceNumber = new TRN();
+        foreignCurrencyInformation = new CUR();
+        receiverIdentification = new REF();
+        versionIdentification = new REF();
+        productionDate = new DTM();
     }
 
     public ClaimPaymentTransaction(String s) {
@@ -22,7 +37,30 @@ public class ClaimPaymentTransaction extends Transaction {
     }
 
     private void parseContent() {
+
         System.out.println(getContent());
+
+        StringBuilder builder = new StringBuilder();
+        StringQueue stringQueue = new StringQueue(getContent());
+
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("BPR"))
+            financialInformation = new BPR(stringQueue.getNext());
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("TRN"))
+            reassociationTraceNumber = new TRN(stringQueue.getNext());
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("CUR"))
+            foreignCurrencyInformation = new CUR(stringQueue.getNext());
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("REF"))
+            receiverIdentification = new REF(stringQueue.getNext());
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("REF"))//todo check order
+            versionIdentification = new REF(stringQueue.getNext());
+        if (stringQueue.hasNext() && stringQueue.peekNext().startsWith("DTM"))
+            productionDate = new DTM(stringQueue.getNext());
+
+        while (stringQueue.hasNext()) {
+            builder.append(stringQueue.getNext());
+        }
+
+        System.out.println(builder.toString());
     }
 
     public void loadDefinition() {
@@ -30,4 +68,6 @@ public class ClaimPaymentTransaction extends Transaction {
         messagesDefinition.add(getTransactionSetHeader());
         messagesDefinition.add(getTransactionSetTrailer());
     }
+
+
 }
